7430014e014a3f599122b884494d2e0c0d1253dd
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-11-30 00:27:58 +0600

    Project setup done

--- Scraping.py
+++ Scraping.py
@@ -1,1 +1,9 @@
-# Here i will scraping data from hacker News 
+# Here i will scraping data from hacker News
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://news.ycombinator.com/news')
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup.find('title'))
