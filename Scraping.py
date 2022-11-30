46d2d2521ca16dd77dbece07695c173c21a17a03
DevXChardike <chardikeltd@gmail.com>
2022-11-30 11:05:33 +0600

    Get hacker news first pages points

--- Scraping.py
+++ Scraping.py
@@ -2,14 +2,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://news.ycombinator.com/news')
-
-soup = BeautifulSoup(response.text, 'html.parser')
+res = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(res.text, 'html.parser')
 
 # print(soup.find(id="score_33788208"))
 # print(soup.select('.score'))
 # print(soup.select('#score_33788208'))
-link = soup.select('.titleline')[0]
-vote = soup.select('.score')[0]
-print(link, vote)
-# print(link)
+
+
+links = soup.select('a')
+subline = soup.select('.subline')
+# print(links, votes)
+
+
+def createCustomNews(links, subline):
+    h_news = []
+
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        # points = votes[idx].getText()
+
+        vote =subline[idx].select('.score')
+        
+        if len(vote): 
+            # to replace points to empty 
+            points = int(vote[0].getText().replace(' points', ''))   
+            print(points)
+
+        #create here a dictionary to create a tile and link list 
+        h_news.append({'title': title, 'links': href})
+    
+    return h_news
+
+createCustomNews(links, subline)
+# print(obj)
+
